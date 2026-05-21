fa2ee9dd91230261c2c25f7aada0efc7b96a98b2
Sumon Debnath <debnathsumon1708@gmail.com>
2026-05-21 18:45:21 +0600

    added and remove in vercel.json, settings.py

--- creatifinity_blog/settings.py
+++ creatifinity_blog/settings.py
@@ -139,10 +139,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR/'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = BASE_DIR/'media'
 
 # Security and headers
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
